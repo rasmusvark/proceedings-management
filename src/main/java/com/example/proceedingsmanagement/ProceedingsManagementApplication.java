2068a6f38a35e7e5fcdc342e33ed57eaef0ac0d6
rasmusvark <rasmusvark@gmail.com>
2024-04-24 12:12:21 +0300

    Initial commit

---
+++ src/main/java/com/example/proceedingsmanagement/ProceedingsManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.proceedingsmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProceedingsManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProceedingsManagementApplication.class, args);
+	}
+
+}
